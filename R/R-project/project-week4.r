58f126e07c3e65cb100d085084c5b75e90003125
Ahmed Basha <a.ahmadbasha@hotmail.com>
2021-04-21 16:07:32 +0300

    Day-4 R project

--- R/R-project/project-week4.r
+++ R/R-project/project-week4.r
@@ -1,5 +1,13 @@
+#Lama Alzahrani
+#Ruba Alkhattabi
+#Reema Alotaibi
+#Ahmed Altowairqi
 
-
+# Potential Questions to Answer:
+# 1. Create snacks that the customers can buy and randomize who buys which snack
+# 2. Pretend you own multiple theaters and run two simulations to represent each theater and plot the results
+# 3. Create conditional statements for movies that may be PG-13 and children are not allowed to watch
+# Cost for adults and children
 ticket_cost <- 20
 ticket_cost_child <- 10 
 movies <- c('The Lion King', 'The Dark Knight', 'The Lord Of The Ring', 'Star Wars', 'Toy Story')  # List 5 of your favorite movies
@@ -8,18 +16,23 @@ seats <-  100# How many seats does each theater hold
 week_days <- rep(0, 7)  # Store totals for each day
 currentTotal <-0
 total<- 0
+snack <- c("popcorn", "coke", "candy", "pepsi", "nachos")
 
 
 # iterate through the week
 for (i in week_days) { 
+  # iterate through the amount of screens on a particular day
   for (j in 1:screens) {
+    # Calculate  how many adults and children are watching the movie
     visitors_adults <- sample(seats, 1)
     visitors_children <- sample(abs(visitors_adults-seats),1)
-   # print(c("adult %d", visitors_adults*20))
-   # print(c("children %d", visitors_children*20))
+    
+    # Calculate the revenue for adults and children for a day and screen 
     currentTotal <- currentTotal + visitors_adults*20 + visitors_children*10
   }
+  # Calculate revenue for the dat 
     print(currentTotal) 
+    # for the total of days , Save total to the corresponding day
     total <<- c(total , currentTotal)
     currentTotal <-0
 
@@ -31,24 +44,35 @@ total <- total[-1]
 names(total) <- c("M", "T", "W", "Th","Fr", "Sa" , "su")
 
 
-typeof(total)
 
-
+# Make a barchart showing total revenue per day
 barplot(
   total,
   main = "Title",
   xlab = " days",
   ylab = "revenue",
   legend = rownames(title),
-  col = c("red"),
-  #horiz = TRUE,
-  #args.legend = list(x= "bottomleft")
-  # Change the placement of your legend by using:
-  # “bottomright”, “bottom”, “bottomleft”, “left”, “topleft”, “top”, 
-  # “topright”, “right”, “center”
+  col = c("red")
 )
 
-# the point shape 
+# Make any other chart
 plot(total, type="o", col="blue")
+# The hieghst day
+paste("The highest revenue is" , max(total), "on" , names(which.max(total)))
 
-max(total)
+###################
+###################
+###################
+snacksChildren <- sample(snack, visitors_children , replace = TRUE)
+snacksAdults<- sample(snack, visitors_adults , replace = TRUE)
+
+
+table(snacksChildren)
+table(snacksAdults)
+
+
+
+
+
+
+
